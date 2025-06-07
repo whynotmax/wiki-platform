c160efb1ebb37d8b48a0395698a16966c3467f2b
KeineSecrets <miethe.max@web.de>
2025-06-07 17:28:32 +0200

    impl: refactor command registration to use updated configuration keys and support aliases and permissions

--- minecraft-plugin/src/main/java/dev/mzcy/minecraft/WikiPlugin.java
+++ minecraft-plugin/src/main/java/dev/mzcy/minecraft/WikiPlugin.java
@@ -2,13 +2,16 @@ package dev.mzcy.minecraft;
 
 
 import dev.mzcy.minecraft.client.WikiApiClient;
+import dev.mzcy.minecraft.command.WikiCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.Field;
+import java.util.List;
 
 public class WikiPlugin extends JavaPlugin {
 
@@ -38,14 +41,13 @@ public class WikiPlugin extends JavaPlugin {
         boolean enableCommand = getConfig().getBoolean("plugin.enable-command", true);
 
         if (enableCommand) {
-            String commandName = getConfig().getString("plugin.command.command-name", "wiki");
+            String commandName = getConfig().getString("plugin.command.name", "wiki");
             String description = getConfig().getString("plugin.command.description", "Get a wiki entry by ID");
             String usage = getConfig().getString("plugin.command.usage", "/wiki <id>");
+            List<String> aliases = getConfig().getStringList("plugin.command.aliases");
+            @Nullable String permission = getConfig().getString("plugin.command.permission");
 
-            Command command = null; //Use the command name to get the command instance
-            command.setDescription(description);
-            command.setUsage(usage);
-
+            Command command = new WikiCommand(apiClient, commandName, description, usage, aliases, permission);
             commandMap.register("wiki", command);
 
             getLogger().info("Command /" + commandName + " enabled.");
