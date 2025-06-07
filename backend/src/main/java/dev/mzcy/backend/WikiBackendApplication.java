85bf7f2ccb136d80289a81db843c910c0400b6d1
KeineSecrets <miethe.max@web.de>
2025-06-07 16:58:10 +0200

    impl: add main application class for Wiki backend

---
+++ backend/src/main/java/dev/mzcy/backend/WikiBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.mzcy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiBackendApplication.class, args);
+    }
+
+}
